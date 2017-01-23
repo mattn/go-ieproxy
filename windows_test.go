95f685afb7305ebd2f64fd13f8e059644efea3e5
oliverpool <oliverpool@hotmail.fr>
2017-01-23 10:19:03 +0100

    add windows tests

--- windows_test.go
+++ windows_test.go
@@ -9,18 +9,94 @@ import (
 
 func TestParseRegedit(t *testing.T) {
 
+	emptyMap := make(map[string]string)
+	catchAllMap := make(map[string]string)
+	catchAllMap[""] = "127.0.0.1"
+	multipleMap := make(map[string]string)
+	multipleMap["http"] = "127.0.0.1"
+	multipleMap["ftp"] = "128"
+	multipleMapWithCatchAll := make(map[string]string)
+	multipleMapWithCatchAll["http"] = "127.0.0.1"
+	multipleMapWithCatchAll["ftp"] = "128"
+	multipleMapWithCatchAll[""] = "129"
+
 	parsingSet := []struct {
 		in  regeditValues
 		out ProxyConf
 	}{
 		{
-			in: regeditValues{
-				ProxyServer:   "",
-				ProxyOverride: "",
-				ProxyEnable:   0,
-				AutoConfigURL: "",
+			in: regeditValues{},
+			out: ProxyConf{
+				Static: StaticProxyConf{
+					Protocols: emptyMap, // to prevent it being <nil>
+				},
 			},
-			out: ProxyConf{},
+		},
+		{
+			in: regeditValues{
+				ProxyServer: "127.0.0.1",
+			},
+			out: ProxyConf{
+				Static: StaticProxyConf{
+					Protocols: catchAllMap,
+				},
+			},
+		},
+		{
+			in: regeditValues{
+				ProxyServer: "http=127.0.0.1;ftp=128",
+			},
+			out: ProxyConf{
+				Static: StaticProxyConf{
+					Protocols: multipleMap,
+				},
+			},
+		},
+		{
+			in: regeditValues{
+				ProxyServer: "http=127.0.0.1;ftp=128;129",
+			},
+			out: ProxyConf{
+				Static: StaticProxyConf{
+					Protocols: multipleMapWithCatchAll,
+				},
+			},
+		},
+		{
+			in: regeditValues{
+				ProxyOverride: "example.com;microsoft.com",
+			},
+			out: ProxyConf{
+				Static: StaticProxyConf{
+					Protocols: emptyMap,
+					NoProxy:   "example.com,microsoft.com",
+				},
+			},
+		},
+		{
+			in: regeditValues{
+				ProxyEnable: 1,
+			},
+			out: ProxyConf{
+				Static: StaticProxyConf{
+					Active:    true,
+					Protocols: emptyMap,
+				},
+			},
+		},
+		{
+			in: regeditValues{
+				AutoConfigURL: "localhost/proxy.pac",
+			},
+			out: ProxyConf{
+				Static: StaticProxyConf{
+					Protocols: emptyMap,
+				},
+				Automatic: AutomaticProxyConf{
+					Active: true,
+					URL:    "localhost/proxy.pac",
+				},
+			},
 		},
 	}
 
