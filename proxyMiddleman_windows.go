af7761ab9952157d3759ff1fa2b0fd162b9e9c51
Martin Multhaupt <m.multhaupt@setlog.com>
2019-06-27 18:21:47 +0200

    Prefer static over automatic config when it is active, as even Internet Explorer itself does it this way.

--- proxyMiddleman_windows.go
+++ proxyMiddleman_windows.go
@@ -15,15 +15,6 @@ func proxyMiddleman() func(req *http.Request) (i *url.URL, e error) {
 	if envcfg.HTTPProxy != "" || envcfg.HTTPSProxy != "" {
 		// If the user manually specifies environment variables, prefer those over the Windows config.
 		return http.ProxyFromEnvironment
-	} else if conf.Automatic.Active {
-		// If automatic proxy obtaining is specified
-		return func(req *http.Request) (i *url.URL, e error) {
-			host := conf.Automatic.FindProxyForURL(req.URL.String())
-			if host == "" {
-				return nil, nil
-			}
-			return &url.URL{Host: host}, nil
-		}
 	} else if conf.Static.Active {
 		// If static proxy obtaining is specified
 		prox := httpproxy.Config{
@@ -34,6 +25,15 @@ func proxyMiddleman() func(req *http.Request) (i *url.URL, e error) {
 
 		return func(req *http.Request) (i *url.URL, e error) {
 			return prox.ProxyFunc()(req.URL)
+		}
+	} else if conf.Automatic.Active {
+		// If automatic proxy obtaining is specified
+		return func(req *http.Request) (i *url.URL, e error) {
+			host := conf.Automatic.FindProxyForURL(req.URL.String())
+			if host == "" {
+				return nil, nil
+			}
+			return &url.URL{Host: host}, nil
 		}
 	} else {
 		// Final fallthrough case; use the environment variables.
