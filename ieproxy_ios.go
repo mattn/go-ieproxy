7f663e7be65e9e129d6a16ea5d0c7a799756dcf9
liwei <liwei-8466@qq.com>
2024-05-08 10:42:04 +0800

    fix: adapt ios

---
+++ ieproxy_ios.go
@@ -0,0 +1,12 @@
+package ieproxy
+
+func getConf() ProxyConf {
+	return ProxyConf{}
+}
+
+func reloadConf() ProxyConf {
+	return getConf()
+}
+
+func overrideEnvWithStaticProxy(pc ProxyConf, setenv envSetter) {
+}
